b4d25f416319bc81e494f3ad704142c0f2d0302a
Jorge Vargas <luisvardez@gmail.com>
2024-09-18 17:18:45 -0600

    estructura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PointsComponent } from './components/points/points.component';
+import { QrLectorComponent } from './components/qr-lector/qr-lector.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component:LoginComponent },
+    { path:'registro', component:RegistroComponent },
+    { path:'points', component: PointsComponent},
+    { path: 'lector', component: QrLectorComponent},
+    { path: '**', redirectTo: '/' }
+];
